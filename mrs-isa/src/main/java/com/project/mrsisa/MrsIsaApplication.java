e19967982423bd6c871b81c8b4d383c17206a4c2
Milica Petrovic <mpetrovic764307@gmail.com>
2022-04-04 13:37:42 +0200

    #1.2 Project initialization

---
+++ mrs-isa/src/main/java/com/project/mrsisa/MrsIsaApplication.java
@@ -0,0 +1,13 @@
+package com.project.mrsisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrsIsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrsIsaApplication.class, args);
+	}
+
+}
