3373b25d2cb61b724038c387ca84c5b61cf7ac77
VioletaSW32 <erdelji.sw32.2019@uns.ac.rs>
2022-04-26 10:55:33 +0200

    3.3.b.2.2 Login controller - controller

---
+++ mrs-isa/src/main/java/com/project/mrsisa/controller/AuthenticationController.java
@@ -0,0 +1,61 @@
+package com.project.mrsisa.controller;
+
+import com.project.mrsisa.domain.User;
+import com.project.mrsisa.dto.JwtAuthenticationRequest;
+import com.project.mrsisa.dto.UserRequest;
+import com.project.mrsisa.dto.UserTokenState;
+import com.project.mrsisa.exception.ResourceConflictException;
+import com.project.mrsisa.service.UserService;
+import com.project.mrsisa.util.TokenUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.http.HttpServletResponse;
+
+//Kontroler zaduzen za autentifikaciju korisnika
+@RestController
+@RequestMapping(value = "/book/site", produces = MediaType.APPLICATION_JSON_VALUE)
+public class AuthenticationController {
+
+	@Autowired
+	private TokenUtils tokenUtils;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<UserTokenState> createAuthenticationToken(
+			@RequestBody JwtAuthenticationRequest authenticationRequest, HttpServletResponse response) {
+
+		// if email and password not valid AuthenticationException
+		// if you want to add aditional params you do it here
+		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+				authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+
+		// valid credentials
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		// create token for user
+		User user = (User) authentication.getPrincipal();
+		String jwt = tokenUtils.generateToken(user.getUsername());
+		int expiresIn = tokenUtils.getExpiredIn();
+
+		// return created token
+		return ResponseEntity.ok(new UserTokenState(jwt, expiresIn));
+	}
+
+}
