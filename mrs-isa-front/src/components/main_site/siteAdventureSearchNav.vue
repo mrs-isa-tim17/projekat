886567a624e13a414235890e009c50242e7857e3
VioletaSW32 <erdelji.sw32.2019@uns.ac.rs>
2022-05-15 23:04:18 +0200

    #3.20.b.2.7 - 1 Adventure filtering - adequate validation on front

--- mrs-isa-front/src/components/main_site/siteAdventureSearchNav.vue
+++ mrs-isa-front/src/components/main_site/siteAdventureSearchNav.vue
@@ -8,9 +8,7 @@
             Od datuma
           </div>
           <div class="col-4">
-            <input type="date" id="start" name="trip-start"
-                   value="2018-07-22"
-                   min="2018-01-01" max="2030-12-31">
+            <Datepicker v-model="dateFrom" style="width: 130px;"></Datepicker>
           </div>
         </div>
       </li>
@@ -20,10 +18,8 @@
             Do datuma
           </div>
           <div class="col-4">
-            <input type="date" id="start" name="trip-start"
-                   value="2018-07-22"
-                   min="2018-01-01" max="2030-12-31">
-          </div>
+            <Datepicker v-model="dateUntil" style="width: 130px;"></Datepicker>
+            </div>
         </div>
       </li>
       <li class="nav-item  py-2" style="display: inline; width: 250px;" >
@@ -32,8 +28,8 @@
             Ocena
           </div>
           <div class="col-3">
-            <select id="ratingBy" class="form-select" style="width: 70px"  aria-label="Default select1">
-              <option selected=""></option>
+            <select id="ratingBy" @change="ratingRelOpChanged" class="form-select" style="width: 70px"  aria-label="Default select1">
+              <option value="0" selected=""></option>
               <option value="1">==</option>
               <option value="2">&#60;=</option>
               <option value="3">&#60;</option>
@@ -42,8 +38,8 @@
             </select>
           </div>
           <div class="col-4">
-            <select id="rating" class="form-select" style="width: 120px" aria-label="Default select2">
-              <option selected=""></option>
+            <select @change="ratingChanged" id="rating" class="form-select" style="width: 120px" aria-label="Default select2">
+              <option value="0" selected=""></option>
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -61,8 +57,8 @@
           </div>
           <div class="col-3">
 
-            <select id="ratingBy" class="form-select" style="width: 70px;"  aria-label="Default select1">
-              <option selected=""></option>
+            <select @change="capacityRelOpChanged" id="capacityRelOpId" class="form-select" style="width: 70px;"  aria-label="Default select1">
+              <option value="0" selected=""></option>
               <option value="1">==</option>
               <option value="2">&#60;=</option>
               <option value="3">&#60;</option>
@@ -72,7 +68,7 @@
           </div>
           <div class="col-4">
 
-            <input type="text" style="width: 120px;  height: 38px;">
+            <input v-model="capacity" type="number" style="width: 120px;  height: 38px;">
           </div>
         </div>
       </li>
@@ -84,8 +80,8 @@
           </div>
           <div class="col-3">
 
-            <select id="ratingBy" class="form-select" style="width: 70px;"  aria-label="Default select1">
-              <option selected=""></option>
+            <select id="priceRelOpId" @change="priceRelOpChanged" class="form-select" style="width: 70px;"  aria-label="Default select1">
+              <option value="0" selected=""></option>
               <option value="1">==</option>
               <option value="2">&#60;=</option>
               <option value="3">&#60;</option>
@@ -95,35 +91,183 @@
           </div>
           <div class="col-4">
 
-            <input type="text" style="width: 120px;  height: 38px;">
+            <input v-model="price" type="number" style="width: 120px;  height: 38px;">
           </div>
         </div>
       </li>
       <li class="nav-item py-2" style="display: inline; width: 120px;" >
-        Adresa
-        <open-maps :lon="lon" :lat="lat" style="width: 250px;"></open-maps>
+
+        <div class="row">
+          <div class="col-8">Adresa</div>
+          <div class="col-2">
+            <button style="width: 150px;" @click="resetLocation"> Poništi lokaciju </button> </div>
+        </div>
+        <open-maps :key="rerenderMapKey" @coordinate-changed="updateCoordinats" :lon="lon" :lat="lat" style="width: 250px;"></open-maps>
       </li>
 
       <li class="nav-item pt-3">
-        <button style="margin-left: 80px;">Pretraži</button>
+        <button @click="filterEntity" style="margin-left: 80px;">Pretraži</button>
       </li>
     </ul>
   </div>
 
+  <div id="liveToastDiv" class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+    <div style="background-color: #F18F86;" id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="toast-header">
+        <strong class="me-auto">Upozorenje</strong>
+        <button type="button" @click.prevent="closeToast" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+      </div>
+      <div class="toast-body text-black">
+        {{ message }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import OpenMaps from "@/components/VueMaps";
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
 export default {
   name: "siteAdventureSearchNav",
   components: {
     OpenMaps,
+    Datepicker
+  },
+  methods: {
+    updateCoordinats(lon, lat){
+      this.lon = lon;
+      this.lat = lat;
+    },
+    resetLocation(){
+      this.lon = 0;
+      this.lat = 0;
+      this.rerenderMapKey++;
+      console.log(this.lon, this.lat);
+    },
+    closeToast(){
+      this.message = "";
+      document.getElementById("liveToast").classList.remove("show");
+      document.getElementById("liveToast").classList.add("hide");
+    },
+    showToast(){
+      document.getElementById("liveToast").classList.remove("hide");
+      document.getElementById("liveToast").classList.add("show");
+    },
+    priceRelOpChanged(){
+      this.priceRelOp = this.getRelOp(document.getElementById("priceRelOpId").value);
+    },
+    capacityRelOpChanged(){
+      this.capacityRelOp = this.getRelOp(document.getElementById("capacityRelOpId").value);
+    },
+    ratingRelOpChanged(){
+      this.ratingRelOp = this.getRelOp(document.getElementById("ratingBy").value);
+    },
+    ratingChanged(){
+      this.rating =document.getElementById("rating").value;
+    },
+    getRelOp(value){
+      switch (value){
+        case "0":
+          return "";
+        case "1":
+          return  "==";
+        case "2":
+          return "<=";
+        case "3":
+          return "<";
+        case "4":
+          return ">=";
+        case "5":
+          return ">";
+      }
+    },
+    addMessage(msg){
+      if (this.message != "")
+        return;
+      this.message += msg;
+    },
+    checkDates(){
+      if (this.dateFrom !== null && this.dateUntil === null){
+        this.addMessage("Dat je početni datum, ali krajnji datum nije dat.");
+      }
+      if (this.dateFrom === null && this.dateUntil !== null){
+        this.addMessage("Dat je krajnji datum, ali početni datum nije dat.");
+      }
+      if (this.dateFrom !== null && this.dateUntil !== null && this.dateFrom > this.dateUntil){
+        this.addMessage("Početni datum je veći od krajnjeg datuma");
+        this.dateFrom = null;
+        this.dateUntil = null;
+      }
+    },
+    checkRating(){
+      if (this.rating !== "" && this.ratingRelOp === ""){
+        this.addMessage("Niste dali relacioni operator za ocenu.");
+      }
+      if (this.rating === "" && this.ratingRelOp !== ""){
+        this.addMessage("Krenuli ste da date opciju za ocenu, pritom niste završili.");
+      }
+    },
+    checkCapacity(){
+      if (this.capacity !== "" && this.capacityRelOp === ""){
+        this.addMessage("Niste dali relacioni operator za kapacitet.");
+      }
+      if (this.capacity === "" && this.capacityRelOp !== ""){
+        this.addMessage("Krenuli ste da date opciju za kapacitet, pritom niste završili.");
+      }
 
+    },
+    checkPrice(){
+      if (this.price !== "" && this.priceRelOp === ""){
+        this.addMessage("Niste dali relacioni operator za cenu.");
+      }
+      if (this.price === "" && this.priceRelOp !== ""){
+        this.addMessage("Krenuli ste da date opciju za cenu, pritom niste završili.");
+      }
+
+    },
+    filterEntity(){
+      this.checkDates();
+      this.checkRating();
+      this.checkCapacity();
+      this.checkPrice();
+      console.log(this.message);
+      if (this.message !== "")
+        this.showToast();
+      let filterDto = {
+        longitude: this.lon,
+        latitude: this.lat,
+        dateFrom: this.dateFrom,
+        dateUntil: this.dateUntil,
+        ratingRelOp: this.ratingRelOp,
+        rating: this.rating,
+        capacityRelOp: this.capacityRelOp,
+        capacity: this.capacity,
+        priceRelOp: this.priceRelOp,
+        price: this.price
+      }
+      this.$emit('filter', filterDto);
+      console.log(filterDto);
+    }
   },
   data(){
     return{
       lon: 2,
-      lat: 9
+      lat: 9,
+      dateFrom: null,
+      dateUntil: null,
+
+      ratingRelOp: "",
+      rating: "",
+
+      capacityRelOp: "",
+      capacity: "",
+
+      priceRelOp: "",
+      price: "",
+
+      message: "",
+      rerenderMapKey: 0
     }
   }
 }
