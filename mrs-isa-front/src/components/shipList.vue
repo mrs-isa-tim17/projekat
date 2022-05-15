6ebe70cef8b2a51ee201a40b2c656ea4d8b55d21
Anastasija Samčović <anastasijas12@gmail.com>
2022-05-15 17:46:12 +0200

    #3.8.1.2 - front for search ships

--- mrs-isa-front/src/components/shipList.vue
+++ mrs-isa-front/src/components/shipList.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="container" >
     <shipOwnerHeader></shipOwnerHeader>
-    <div id="addShip">
-      <button id="add" @click="addShipPage">Dodaj novi brod</button>
+  <div class="row d-flex justify-content-center">
+    <div class="col-8 d-flex justify-content-center">
+      <div class="input-group mb-3 input-icons" >
+        <i class="fa fa-search icon"></i><input placeholder="Pretraži po imenu..." type="search" class="input-field form-control" aria-label="Text input with dropdown button" v-model="search"  @keyup="filtered">
+      </div>
+      <button class="btn-sm "  @click="addShipPage"><i class="fa fa-ship"></i> Dodaj novi brod</button>
     </div>
+  </div>
     <div class="p-2" v-for="(s) in allShips"  :key="s.id">
       <shipElement :ship="s"></shipElement>
     </div>
@@ -21,31 +26,57 @@ export default {
 
     return {
       allShips: null,
-      myKey: 1
+      myKey: 1,
+      search:""
     }
   },
-  mounted(){
-    this.coID = JSON.parse(localStorage.user).id;//this.$route.params.id;
-    console.log(this.coID);
-    ShipService.getShipByOwner(this.coID)
-        .then((response)=>{
-          this.allShips = response.data;
-          console.log(this.allShips);
-        })
-  },
+
   methods:{
     addShipPage(){
       this.$router.push('/ship/add');
-    }
+    },
+  filtered(){
+    this.coID = JSON.parse(localStorage.user).id;//this.$route.params.id;
+    console.log(this.coID);
+    ShipService.getShipByOwner(this.coID)
+        .then((response) => {
+          if(!this.search) {
+            this.allShips = response.data;
+            console.log(this.allShips);
+          }
+          else{
+            console.log(response.data);
+            this.allShips = response.data.filter(ship =>
+                ship.name.toLowerCase().includes(this.search.toLowerCase())
+            );
+            console.log(this.allShips);
+          }
+        })
+
+  }
+  },
+  created(){
+    this.filtered();
   }
 }
 </script>
 
 <style scoped>
-#add{
-  margin-left:850px;
-  margin-bottom: 30px;
-  margin-top:30px;
-  height:50px;
+
+.input-icons{
+  width: 100%;
+  margin-bottom: 10px;
+  float:left;
+}
+.icon {
+  padding: 10px;
+  min-width: 40px;
+}
+input{
+  margin-right:20px;
+}
+button{
+  width:250px;
+  height:38px;
 }
 </style>
